1fa76ec2fec7430995512ff56adfc56814d87678
Thomas Levine <_@thomaslevine.com>
2015-08-17 23:45:46 -0400

    j

--- R/file.R
+++ R/file.R
@@ -22,4 +22,25 @@ estimate.count <- function(f, pattern = '\n', n = 1000, page.size = 2^14) {
   else if (N <= n)
     stop('File is too small; just read the whole file.')
 
+  total.sample <- function(where) {
+    seek(con, where)
+    length(strsplit(readChar(con, page.size), pattern))
+  }
+  wheres <- file.start + sort(sample.int(0, N)) * page.size
+  total.samples <- sapply(wheres, total.sample)
+  
+
+    ts = list(map(f, sorted(sample(range(0, N), n))))
+    E_t_sample = sum(ts) / n
+    Var_t_sample = sum((t - E_t_sample) ** 2 for t in ts) / (n - 1)
+
+    # 99% gaussian confidence interval
+    z = 2.575829
+
+    # Levy & Lemeshow, page 51
+    fpc = (N - n) / N
+    E_t_population = N * E_t_sample * (page_size / actual_page_size)
+    Var_t_population = (N ** 2) * fpc * Var_t_sample / n
+    SE_t_population = Var_t_population ** 0.5
+
 }
