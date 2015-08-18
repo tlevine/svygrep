017ae9a66dda503788830ae0c56a9c109e445836
Thomas Levine <_@thomaslevine.com>
2015-08-17 23:36:26 -0400

    n and N

--- R/file.R
+++ R/file.R
@@ -6,7 +6,14 @@
 #' @param Each observation within the sample is this many bytes long.
 #' @return survey object of some sort
 estimate.count <- function(f, pattern = '\n', n = 1000, page.size = 2^14) {
+  if (n < 0)
+    stop('Sample size must be greater than zero.')
+
   con <- file(f, open = 'rb')
   file.start <- seek(con, where = 0, origin = 'end')
   file.end <- seek(con)
+
+  # Population size, ignoring the last page for now.
+  # To do: Weight the last page lower, proportional to its size.
+  N <- as.integer((file.end - file.start) / page.size)
 }
