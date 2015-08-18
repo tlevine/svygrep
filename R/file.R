713a33652cd79b12a15dc2075eaae5620a03d05b
Thomas Levine <_@thomaslevine.com>
2015-08-17 23:34:23 -0400

    seeking

--- R/file.R
+++ R/file.R
@@ -5,4 +5,8 @@
 #' @param n Sample size
 #' @param Each observation within the sample is this many bytes long.
 #' @return survey object of some sort
-estimate.count <- function(file, pattern = '\n', n = 1000, page.size = 2^14)
+estimate.count <- function(f, pattern = '\n', n = 1000, page.size = 2^14) {
+  con <- file(f, open = 'rb')
+  file.start <- seek(con, where = 0, origin = 'end')
+  file.end <- seek(con)
+}
